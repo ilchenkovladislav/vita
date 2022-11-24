7356b80597c5258d90e5cd7ce77f438c38445053
Ilchenko <ilchenkowd@gmail.com>
2022-11-24 10:38:03 +0500

    Removed unnecessary styles and added a component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import FormAdding from "./components/FormAdding/FormAdding"
 
 function App() {
-
   return (
-    <div className="App"></div>
+    <div className="App">
+      <FormAdding />
+    </div>
   );
 }
 
