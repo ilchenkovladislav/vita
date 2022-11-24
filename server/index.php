5549986c29b77662d99fb963255e09707a15b991
Ilchenko <ilchenkowd@gmail.com>
2022-11-24 12:13:08 +0500

    Added headers for cors

--- server/index.php
+++ server/index.php
@@ -1,11 +1,13 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: *");
 
 require("DbConnect.php");
 
 $conn = new DbConnect;
 $db = $conn->connect();
 $method = $_SERVER['REQUEST_METHOD'];
-switch($method) {
+switch ($method) {
     case 'GET':
         $sql = "SELECT * FROM pages";
         $stmt = $db->prepare($sql);
@@ -18,7 +20,7 @@ switch($method) {
         $sql = "INSERT INTO pages(id, page) values(null, :page)";
         $stmt = $db->prepare($sql);
         $stmt->bindParam(':page', $json);
-        if($stmt->execute()) {
+        if ($stmt->execute()) {
             $data = ['status' => 1, 'message' => "Record successfully created"];
         } else {
             $data = ['status' => 0, 'message' => "Failed to create record."];
