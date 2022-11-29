c9094ed4b1da92cb2668ec7c7cd32d4e6e716f69
Ilchenko <ilchenkowd@gmail.com>
2022-11-29 12:01:23 +0500

    I made the form a pop-up window and moved removeImage

--- client/src/components/FormAdding/FormAdding.jsx
+++ client/src/components/FormAdding/FormAdding.jsx
@@ -3,100 +3,112 @@ import { useState } from "react";
 import Dropzone from "../Dropzone/Dropzone";
 import "./FormAdding.scss";
 
-export default function FormAdding() {
-  const [data, setData] = useState({
+export default function FormAdding(props) {
+  const [section, setSection] = useState({
     title: "",
     images: [],
     comment: "",
   });
 
-  const [isSubmitted, setIsSubmitted] = useState(false);
-
-  const clearForm = () => {
-    setData({
-      title: "",
-      images: [],
-      comment: "",
-    });
-  };
-
   useEffect(() => {
-    if (isSubmitted) {
-      clearForm();
-      alert("Секция добавлена");
-      setIsSubmitted(false);
-    }
-  }, [isSubmitted]);
+    setSection(props.section);
+  }, [props.section]);
 
   const inputHandler = (e) => {
     const { name, value } = e.target;
 
-    setData((prevData) => ({
+    setSection((prevData) => ({
       ...prevData,
       [name]: value,
     }));
   };
 
-  const handleDropzone = useCallback((images) => {
-    setData((prevData) => ({
+  const removeImage = (id) => {
+    const result = [
+      ...section.images.slice(0, id),
+      ...section.images.slice(id + 1),
+    ];
+    updateImages(result);
+  };
+
+  const updateImages = useCallback((images) => {
+    setSection((prevData) => ({
       ...prevData,
       images,
     }));
   }, []);
 
-  const postDataOnServer = async (data) => {
-    let response = await fetch("http://vita/server/", {
-      method: "post",
-      headers: { "Content-Type": "application/json;charset=utf-8" },
-      body: data,
-    });
-
-    let result = await response.json();
-
-    result.status ? setIsSubmitted(true) : alert("Не удалось добавить запись");
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    postDataOnServer(JSON.stringify(data));
+    props.onAdd(props.pageId, section);
+  };
+
+  if (!props.show) {
+    return null;
+  }
+
+  function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+
+    return {
+      top: box.top + window.pageYOffset,
+      right: box.right + window.pageXOffset,
+      bottom: box.bottom + window.pageYOffset,
+      left: box.left + window.pageXOffset,
+    };
+  }
+
+  const formStyle = () => {
+    const form = props.ulishka.current[props.pageId].current;
+
+    let coords = getCoords(form);
+    return { left: coords.right + 30, top: coords.top };
   };
 
   return (
-    <form
-      action="http://vita/server/"
-      method="post"
-      className="formAdding"
-      onSubmit={handleSubmit}
-    >
-      <div>
-        <label htmlFor="title">Заголовок</label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          value={data.title}
-          onChange={inputHandler}
-        />
-      </div>
-      <div>
-        <label>Фотки:</label>
-        <Dropzone images={data.images} handleDropzone={handleDropzone} />
-      </div>
-      <div>
-        <label htmlFor="comment">Комментарии:</label>
-        <textarea
-          name="comment"
-          id="comment"
-          value={data.comment}
-          onChange={inputHandler}
-          cols="30"
-          rows="10"
-        ></textarea>
-      </div>
-      <button className="formAdding__btn" type="submit">
-        Добавить секцию
-      </button>
-    </form>
+    <div className="form__back" onClick={props.onClose}>
+      <form
+        action="http://vita/server/"
+        method="post"
+        className="formAdding"
+        onSubmit={handleSubmit}
+        onClick={(e) => e.stopPropagation()}
+        style={formStyle()}
+      >
+        <div>
+          <label htmlFor="title">Заголовок</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={section.title}
+            onChange={inputHandler}
+          />
+        </div>
+        <div>
+          <label>Фотки:</label>
+          <Dropzone
+            images={section.images}
+            onRemoveImg={removeImage}
+            updateImages={updateImages}
+          />
+        </div>
+        <div>
+          <label htmlFor="comment">Комментарии:</label>
+          <textarea
+            name="comment"
+            id="comment"
+            value={section.comment}
+            onChange={inputHandler}
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+        <button className="formAdding__btn" type="submit">
+          Добавить секцию
+        </button>
+      </form>
+    </div>
   );
 }
