90325f445107802da4517f85cb335a3783b28f6d
Ilchenko <ilchenkowd@gmail.com>
2023-01-21 17:02:25 +0500

    Added ErrorBoundary

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { AdminPanel } from "./components/AdminPanel/AdminPanel";
 import { UserPage } from "./components/UserPage/UserPage";
 import { Header } from "./components/Header/Header";
 import ThemeProvider from "./providers/ThemeProvider";
+import { ErrorBoundary } from "./components/ErrorBoundary/ErrorBoundary";
 import "./App.scss";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,16 +11,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div style={{ position: "relative" }} className="App">
-      <ThemeProvider>
-        <Header />
-        <BrowserRouter>
-          <Routes>
-            <Route path="admin" element={<AdminPanel />} />
-            <Route path="page/:href" element={<UserPage />} />
-            <Route path="*" element={<h1>Страница не найдена</h1>} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <Header />
+          <BrowserRouter>
+            <Routes>
+              <Route path="admin" element={<AdminPanel />} />
+              <Route path="page/:href" element={<UserPage />} />
+              <Route path="*" element={<h1>Страница не найдена</h1>} />
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </ErrorBoundary>
     </div>
   );
 }
