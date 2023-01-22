f892df16488da1265e981b00d4afd841a9e8d7e6
Ilchenko <ilchenkowd@gmail.com>
2023-01-22 09:25:53 +0500

    changed the errorboundary render

--- client/src/components/ErrorBoundary/ErrorBoundary.jsx
+++ client/src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -3,16 +3,27 @@ import React from "react";
 export class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, errorText: "" };
   }
 
   static getDerivedStateFromError(error) {
-    return { hasError: true };
+    return { hasError: true, errorText: error.toString() };
   }
 
   render() {
     if (this.state.hasError) {
-      return <h1>Что-то пошло не так.</h1>;
+      return (
+        <h1
+          style={{
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          Что-то пошло не так. {this.state.errorText}
+        </h1>
+      );
     }
 
     return this.props.children;
