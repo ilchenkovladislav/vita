16cf62487ac89d3e78601879a6389ceeec122fcf
Ilchenko <ilchenkowd@gmail.com>
2023-01-27 10:19:48 +0500

    fixed the encoding

--- server/DbConnect.php
+++ server/DbConnect.php
@@ -8,7 +8,7 @@ class DbConnect
     public function connect()
     {
         try {
-            $conn = new PDO('mysql:host=' . $this->server . ';dbname=' . $this->dbname, $this->user, $this->pass);
+            $conn = new PDO('mysql:host=' . $this->server . ';dbname=' . $this->dbname, $this->user, $this->pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         } catch (\Exception $e) {
