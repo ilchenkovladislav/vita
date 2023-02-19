f0513d5ff133d99fd047d494f5a4cab6215d75e8
Ilchenko <ilchenkowd@gmail.com>
2023-02-20 01:00:36 +0500

    created page 404

---
+++ client/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import "./NotFoundPage.scss";
+
+export const NotFoundPage = () => {
+  return (
+    <div className="nfp">
+      <h1 className="nfp__h1">404</h1>
+      <p className="nfp__p">страница не найдена</p>
+    </div>
+  );
+};
